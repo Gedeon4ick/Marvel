74308774226078b54fb65c294af843c4e97b3ee9
Gedeon4ick <pavell720@gmaul.com>
2023-01-11 12:02:10 +0600

    first commit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,41 @@
+// файлы для взаимодействия с сервером
+
+// создаем класс, здесь мы не импорттируем компонент и не наследуем ничего, так как класс будет на чистом Js. 
+
+class MarvelService {
+    _apiBase =  'https://gateway.marvel.com:443/v1/public/';
+    _apiKey ='apikey=158c7e484bbe3f594246c0ba7c72b090';
+     // функция для отправки get запросов
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+    // методы которые будут делать запросы
+    getAllCharacters =  async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=96offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
+    }
+    // получение только одного персонажа
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    // метод трансформации данных в нужный нам формат
+    _transformCharacter = (char) => {
+          return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[0].url
+          }
+    }
+}
+
+export default MarvelService;
